75db48a4d404f1bbf528e2089254e4e6edfc8774
Erick <ericktakao28@gmail.com>
2021-08-18 20:36:15 -0300

    feat: create provisory admin dashboard

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+
+import { isAuthenticated } from './services/auth';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -7,6 +9,19 @@ import Ecommerce from './pages/Ecommerce';
 import Home from './pages/Ecommerce/Home';
 import Cart from './pages/Ecommerce/Cart';
 import Profile from './pages/Ecommerce/Profile';
+import Dashboard from './pages/Dashboard';
+import Products from './pages/Dashboard/Products';
+import Details from './pages/Dashboard/Details';
+import Clients from './pages/Dashboard/Clients';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    isAuthenticated() ?
+      <Component {...props} />
+      :
+      <Redirect to='/login' />
+  )} />
+);
 
 export const EcommerceRoutes = () => {
   return (
@@ -18,12 +33,23 @@ export const EcommerceRoutes = () => {
   );
 }
 
+export const DashboardRoutes = () => {
+  return (
+    <Switch>
+      <PrivateRoute exact path="/dashboard/products" component={Products} />
+      <PrivateRoute exact path="/dashboard/products/:productId" component={Details} />
+      <PrivateRoute exact path="/dashboard/clients" component={Clients} />
+    </Switch>
+  )
+}
+
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={Dashboard} />
         <Route path="/" component={Ecommerce} />
       </Switch>
     </BrowserRouter>
